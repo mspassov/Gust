5981c702ddb931a6a5c11cce605ff6780a79cb95
Martin Spassov <mspassov@uwo.ca>
2022-10-16 09:49:06 -0400

    navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { auth } from "../config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { signOut } from "firebase/auth";
+import { FaWind } from "react-icons/fa";
 
 const Navbar = () => {
   const [user, loading, error] = useAuthState(auth);
@@ -13,17 +14,28 @@ const Navbar = () => {
 
   return (
     <nav>
-      <h1>Gust App</h1>
-      {user ? (
-        <Link to="/">
-          <button onClick={logout}>Logout</button>
-        </Link>
-      ) : (
-        <>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </>
-      )}
+      <div className="container nav-container">
+        <div className="logo-container">
+          <FaWind style={{ height: "30px", width: "30px", color: "#CA1551" }} />
+          <h1>Gust Social</h1>
+        </div>
+        {user ? (
+          <Link to="/">
+            <div className="login">
+              <button onClick={logout}>Logout</button>
+            </div>
+          </Link>
+        ) : (
+          <div className="link-container">
+            <div className="login">
+              <Link to="/login">Login</Link>
+            </div>
+            <div className="register">
+              <Link to="/register">Register</Link>
+            </div>
+          </div>
+        )}
+      </div>
     </nav>
   );
 };
