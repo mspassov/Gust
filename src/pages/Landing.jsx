53bfe5ba446d3c9bd69c714ce040ca26c8285b19
Martin Spassov <mspassov@uwo.ca>
2022-11-26 17:40:25 -0500

    added landing page styling

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import LandingPicture from "../images/landing-page-picture.jpg"
 
 const Landing = () => {
   return (
     <div>
       <Navbar />
-      <h2>Landing Page</h2>
+      <div className="container">
+        <div className="landing-container">
+          <img src={LandingPicture} alt="" className="landing-img"/>
+          <div className="landing-text">
+            <h3>What <span className="adventure">adventure</span> will you share?</h3>
+            <p>Gust is the newest social media platform which allows you to show the world all of the cool places you've been to. Start sharing today!</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
